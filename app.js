5178761fef4b2d651be45921cba242252eea1e6e
RyoAizawa <aizawaryou6419@gmail.com>
2023-08-01 17:49:13 +0900

    完成

--- app.js
+++ app.js
@@ -26,8 +26,17 @@ app.get("/favicon.ico", (req, res) => {
     res.status(204);
 });
 
-/*
+// フォーム送信された値の改行コードを変換するメソッド
+function convert(jsonString) {
+    return jsonString
+        .replace(/(\r\n)/g, "\n")
+        .replace(/(\r)/g, "\n")
+        .replace(/(\n)/g, "\\n");
+}
 
+/*
+    トップページ表示
+    データベースからスケジュールのデータを全て取得し表示
 */
 app.get("/", (req, res) => {
     const sql =
@@ -38,12 +47,44 @@ app.get("/", (req, res) => {
     });
 });
 
+/*
+    新規スケジュール投稿
+    登録したらトップページをリダイレクト
+*/
 app.post("/post", (req, res) => {
+    req.body.content = convert(req.body.content);
     const sql =
         "INSERT INTO calendar SET ?";
     con.query(sql, req.body, function (err, result, fields) {
         if (err) throw err;
-        res.redirect("index");
+        res.redirect("/");
+    });
+});
+
+/*
+    スケジュール削除
+    削除したらトップページをリダイレクト
+*/
+app.get("/delete/:id", (req, res) => {
+    const sql =
+        "DELETE FROM calendar WHERE id = " + req.params.id;
+    con.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        res.redirect("/");
+    });
+});
+
+/*
+    スケジュール更新
+    更新したらトップページをリダイレクト
+*/
+app.post("/update/:id", (req, res) => {
+    req.body.content = convert(req.body.content);
+    const sql =
+        "UPDATE calendar SET ? WHERE id = " + req.params.id;
+    con.query(sql, req.body, function (err, result, fields) {
+        if (err) throw err;
+        res.redirect("/");
     });
 });
 
